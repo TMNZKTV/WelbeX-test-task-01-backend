4272827acbb9d742e9980513506e695fa0503342
TUMEN ZORIKTUEV <69411964+TMNZKTV@users.noreply.github.com>
2021-09-29 15:33:49 +0800

    fixing bugs

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectMongo = async () => {
   return mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
+    useUnifiedTopology: true,
     useNewUrlParser: true,
   });
 };
